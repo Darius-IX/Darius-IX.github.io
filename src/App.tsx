6474cc3935677fd25f13fe937cec9061655509ae
Darius-IX <89026567+Darius-IX@users.noreply.github.com>
2024-09-11 11:49:23 +0200

    first_demo all

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Administration from "./pages/Administration";
+import bindHotkeys from "./bindHotkeys";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  bindHotkeys();
+
+  return (
+    <div className="h-screen">
+      <Navbar></Navbar>
+      <div className="bg-gradient-to-l from-accent-light to-accent-dark flex flex-col justify-between h-screen">
+        <div>
+          <div className="p-12"></div>
+          <div className="bg-gradient-to-l from-accent-light to-accent-dark">
+            <Routes>
+              <Route path="/" Component={Dashboard}></Route>
+              <Route path="/profil" Component={Profile}></Route>
+              <Route path="/verwaltung" Component={Administration}></Route>
+            </Routes>
+          </div>
+          <div className="bg-gradient-to-l from-accent-light to-accent-dark h-full w-full flex-1"></div>
+        </div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
